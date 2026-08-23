5377190c5d33ad8cf53ebd4a35f3298955e0efc8
Anushka21s <anushkashinde210406@gmail.com>
2026-08-23 10:32:55 +0530

    first_agetn_uploaded

--- task.py
+++ task.py
@@ -2,7 +2,7 @@ from crewai import Task
 from agent import researcher
 
 research_task = Task(
-    description="Research the latest trends in Agentic AI freelancing opportunities.",
-    expected_output="A concise report summarizing the latest trends, skills in demand, major freelancing opportunities, and important observations.",
+    description="Research the latest opportunities in remote sensing and gis",
+    expected_output="A concise report summarizing the latest research paper, skills in demand, major freelancing and work opportunities, and important observations.",
     agent=researcher
 )
